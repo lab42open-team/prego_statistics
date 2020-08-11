4ed60bd24169020e0612b4a4c950bd0037c43ab4
prego <prego@hcmr.gr>
2020-08-11 15:26:02 +0300

    removed the 10th column of associations during import

--- descriptives_mgnify.r
+++ descriptives_mgnify.r
@@ -5,7 +5,7 @@ library(Rfast)
 #library(vegan) #needs to installed globally
 ## file loading
 
-mgnify_associations <- read_delim("/data/databases/scripts/gathering_data/mgnify/mgnify_markergene_associations.tsv", delim = "\t", col_names = F)
+mgnify_associations <- read_delim("/data/databases/scripts/gathering_data/mgnify/mgnify_markergene_associations.tsv", delim = "\t", col_names = F) %>% select(-10)
 colnames(mgnify_associations) <- c("type_1","term_1","type_2","term_2","source","evidence","MI","status","url")
 
 mgnify_sample_entity_sources <- read_delim("/data/databases/scripts/gathering_data/mgnify/sample_entity_sources.tsv", delim = "\t", col_names = F)
